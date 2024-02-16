cfb9b4b42f26739d4403845272becc94ba7b83a8
darasimi <abayomiberachal@gmail.com>
2024-02-16 11:04:39 +0100

    assignment added

--- dart_basic/bin/dart_basic.dart
+++ dart_basic/bin/dart_basic.dart
@@ -1,6 +1,36 @@
+// Solution to question 2, 3, 4  and 7
+
+main(){
+
+  // Solution 2
+print('Hello, Voyagers');
+
+// Solution 3
+String myName = 'abayomi darasimi';
+print(myName);
+
+//Solution 4
+// String x = 'x is ';
+// print('${x}10, and ${x}50');
+// 4
+dynamic x ;
+x=10;
+x=50;
+// 5
+var value =10/2;
+print (value.runtimeType);
+// 6
+print (true && true);
+print (false || false);
+print ((true && 1 !=2) || (4 > 3 && 100 < 1));
+print (((10/2)>3)&& ((10%2)==0));
+//Solution to question 
+print(myFunctionToCalcPeremeterOfARectangle(4, 6));
+
+}
 
 
-void main(List<String> arguments) {
-String name="ayo";
-print("my name is $name");
+myFunctionToCalcPeremeterOfARectangle(int l, int w){
+  return  2 * (l+w);
+  
 }
